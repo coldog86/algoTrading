7bda52058a2caaafbde030e1340abab8b1373f98
coldog86 <30303169+coldog86@users.noreply.github.com>
2025-02-03 13:43:56 +1100

    Update GoBabyGo.ps1

--- crypto/GoBabyGo.ps1
+++ crypto/GoBabyGo.ps1
@@ -1,5 +1,10 @@
 [Parameter(Mandatory = $false)][int] $WaitTime
 [Parameter(Mandatory = $false)][int] $Count
+[Parameter(Mandatory = $false)][string] $Username = "coldog86"
+[Parameter(Mandatory = $false)][string] $Repo = 'algoTrading'
+[Parameter(Mandatory = $false)][string] $Branch = 'Beta'
+[Parameter(Mandatory = $false)][string] $Folder = 'crypto'
+[Parameter(Mandatory = $false)][string] $FileName = 'CryptoModule.psm1'
 
 if($null -eq $waitTime){
     $waitTime = 600
@@ -7,6 +12,10 @@ if($null -eq $waitTime){
 if($null -eq $count){
     $count = 0
 } 
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/$($username)/$($repo)/refs/heads/$($branch)/$($folder)/$($fileName)" -OutFile $fileName
+Import-Module .\$fileName -Force -WarningAction Ignore
+
 
 Import-Module 'E:\cmcke\Documents\Crypto\CryptoModule.psm1' -Force -WarningAction Ignore
 $telegramToken = Get-TelegramToken
