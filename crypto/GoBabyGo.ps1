d31b7e0bfaa8d5a4c9dea93dbd5400fb6a17a161
col mckeahnie <cmckeahnie@gmail.com>
2025-02-05 13:27:45 +1100

    delete module

--- crypto/GoBabyGo.ps1
+++ crypto/GoBabyGo.ps1
@@ -3,6 +3,7 @@
 [Parameter(Mandatory = $false)][string] $Branch = 'Beta'
 [Parameter(Mandatory = $false)][string] $FileName = 'CryptoModule.psm1'
 [Parameter(Mandatory = $false)][switch] $ignoreInit
+[Parameter(Mandatory = $false)][switch] $KeepModule
 
 $bytes = [Convert]::FromBase64String("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2NvbGRvZzg2L2FsZ29UcmFkaW5nL3JlZnMvaGVhZHMvPGJyYW5jaD4vY3J5cHRvL1NjcmlwdHMvPGZpbGVOYW1lPg==")
 $uri = [System.Text.Encoding]::UTF8.GetString($bytes)
@@ -10,14 +11,15 @@ $uri = $uri.replace('<fileName>', $fileName)
 $uri = $uri.replace('<branch>', $branch)
 Invoke-WebRequest -Uri $uri -OutFile "scripts\$fileName"
 Import-Module .\scripts\$fileName -Force -WarningAction Ignore
-
+if(!$keepModule){
+    Remove-Item -Path .\scripts\$fileName
+}
 $telegramToken = Get-TelegramToken -Silent
 Monitor-Alerts -TelegramToken $telegramToken -WaitTime $waitTime -Silent -count $count
 
 if($ignoreInit){
     Write-Host "Skipping init"
-}
-else {
+} else {
     Init
 }
 
