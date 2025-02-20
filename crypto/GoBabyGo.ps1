50e9980a87345b5b49fc0f310f7d729c346cbaaa
col mckeahnie <cmckeahnie@gmail.com>
2025-02-20 16:22:38 +1100

    newURI

--- crypto/GoBabyGo.ps1
+++ crypto/GoBabyGo.ps1
@@ -19,8 +19,9 @@ if($pullRepoOnly){
 $bytes = [Convert]::FromBase64String("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2NvbGRvZzg2L2FsZ29UcmFkaW5nL3JlZnMvaGVhZHMvPGJyYW5jaD4vY3J5cHRvL1NjcmlwdHMvPGZpbGVOYW1lPg==")
 $uri = [System.Text.Encoding]::UTF8.GetString($bytes)
 foreach($fileName in $fileNames){
-    $uri = $uri.replace('<fileName>', $fileName)
-    $uri = $uri.replace('<branch>', $branch)
+    $newUri = $uri.replace('<fileName>', $fileName)
+    $newUri = $newUri.replace('<branch>', $branch)
+    $newUri
     Invoke-WebRequest -Uri $uri -OutFile "scripts\$fileName"
     Import-Module .\scripts\$fileName -Force -WarningAction Ignore
     Remove-Item -Path .\scripts\$fileName
