af199281302a790a4b7aab5cf0dd048bb37c14ea
col mckeahnie <cmckeahnie@gmail.com>
2025-02-20 14:28:31 +1100

    gobabygo update

--- crypto/GoBabyGo.ps1
+++ crypto/GoBabyGo.ps1
@@ -14,19 +14,16 @@ Write-Host "Accessing branch: " -NoNewline; Write-Host "$($branch)" -ForegroundC
 if($pullRepoOnly){
     Write-Host "Updating local files only" -ForegroundColor Yellow
 }
+# Import modules
 $bytes = [Convert]::FromBase64String("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2NvbGRvZzg2L2FsZ29UcmFkaW5nL3JlZnMvaGVhZHMvPGJyYW5jaD4vY3J5cHRvL1NjcmlwdHMvPGZpbGVOYW1lPg==")
 $uri = [System.Text.Encoding]::UTF8.GetString($bytes)
-$uri = $uri.replace('<fileName>', $fileName)
-$uri = $uri.replace('<branch>', $branch)
-
-# Import modules
 foreach($fileName in $fileNames){
+    $uri = $uri.replace('<fileName>', $fileName)
+    $uri = $uri.replace('<branch>', $branch)
     Invoke-WebRequest -Uri $uri -OutFile "scripts\$fileName"
     Import-Module .\scripts\$fileName -Force -WarningAction Ignore
     Remove-Item -Path .\scripts\$fileName
 }
-
-$telegramToken = Get-TelegramToken -Silent $silent
 
 if($ignoreInit){
     Write-Host "Skipping init"
@@ -34,6 +31,7 @@ if($ignoreInit){
     Init -Branch $branch
 }
 if(!$pullRepoOnly){
+    $telegramToken = Get-TelegramToken -Silent $silent
     Monitor-Alerts -TelegramToken $telegramToken -Silent $silent -CollectDataOnly $collectDataOnly
 }
 
