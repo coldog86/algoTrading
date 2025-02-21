798edeca0c9ef803749bf8e170a6115e59b2ee33
col mckeahnie <cmckeahnie@gmail.com>
2025-02-21 18:10:07 +1100

    test2

--- crypto/Scripts/StrategyModule.psm1
+++ crypto/Scripts/StrategyModule.psm1
@@ -14,8 +14,8 @@ function Run-BolleringBandStrategy {
     Write-Host "Starting live trading with Bollering Band strategy..." -ForegroundColor Green
     while ($true) {
         $currentPrice = Get-TokenPrice -TokenCode $tokenCode -TokenIssuer $tokenIssuer
-        $currentPrice
-        $TokenName
+        Write-Host "currentPrice = $($currentPrice)"
+        Write-Host "currentPrice = $($TokenName)"
         Log-Price -TokenName $tokenName -TokenPrice $currentPrice # log all the price data for a token 
         break
         if($i -eq 5){
